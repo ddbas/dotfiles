602c6b8f84f01e46ce8cbac5c0548b2161720247
David Bastien <velours.ingrate_00@icloud.com>
2023-03-07 21:33:46 -0500

    new uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,32 +1,144 @@
 #!/usr/bin/env bash
-echo "Uninstall scripts"
-if [ -L ~/.local/bin ]; then
-    rm -rf ~/.local/bin 2> /dev/null
+
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+uninstall_packages=
+uninstall_configurations=
+uninstall_scripts=
+
+help() {
+  cat << EOF
+usage: $0 [OPTIONS]
+
+    --help                Show this message
+    --all                 Uninstall packages, configurations and scripts
+    --[no-]packages       Uninstall packages
+    --[no-]configurations Uninstall configurations
+    --[no-]scripts        Uninstall scripts
+EOF
+}
+
+for opt in "$@"; do
+  case $opt in
+    --help)
+      help
+      exit 0
+      ;;
+    --all)
+      uninstall_packages=1
+      uninstall_configurations=1
+      uninstall_scripts=1
+      ;;
+    --packages)          uninstall_packages=1       ;;
+    --no-packages)       uninstall_packages=0       ;;
+    --configurations)    uninstall_configurations=1 ;;
+    --no-configurations) uninstall_configurations=0 ;;
+    --scripts)           uninstall_scripts=1        ;;
+    --no-scripts)        uninstall_scripts=0        ;;
+    *)
+      echo "unknown option: $opt"
+      help
+      exit 1
+      ;;
+  esac
+done
+
+ask() {
+  while true; do
+    read -p "$1 ([y]/n) " -r
+    REPLY=${REPLY:-"y"} # default to yes
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      return 1
+    elif [[ $REPLY =~ ^[Nn]$ ]]; then
+      return 0
+    fi
+  done
+}
+
+if [ -z "$uninstall_packages" ]; then
+  ask "Do you want to uninstall the packages?"
+  uninstall_packages=$?
 fi
 
-echo "Uninstall configurations"
-if [ -L ~/.zshrc ]; then
-    echo "zsh"
-    rm ~/.zshrc 2> /dev/null
+if [ -z "$uninstall_configurations" ]; then
+  ask "Do you want to uninstall the configurations?"
+  uninstall_configurations=$?
 fi
 
-if [ -L ~/.tmux.conf ]; then
-    echo "tmux"
-    rm ~/.tmux.conf 2> /dev/null
+if [ -z "$uninstall_scripts" ]; then
+  ask "Do you want to uninstall the scripts?"
+  uninstall_scripts=$?
 fi
 
-if [ -L ~/.alacritty.yml ]; then
-    echo "alacritty"
-    rm ~/.alacritty.yml 2> /dev/null
+if [ $uninstall_scripts -eq 1 ]; then
+  echo -e "${GREEN}>>${NC} Uninstalling scripts"
+  if [ -L ~/.local/bin ]; then
+      rm -rf ~/.local/bin 2> /dev/null
+  fi
 fi
 
-if [ -L ~/.gitconfig ] && [ -L ~/.gitmessage ]; then # if symbolic link
-    echo "git"
-    rm ~/.gitconfig 2> /dev/null
-    rm ~/.gitmessage 2> /dev/null
+uninstall_alacritty_config() {
+  if [ -L ~/.alacritty.yml ]; then
+      echo -e "${BLUE}>>${NC} Alacritty"
+      rm ~/.alacritty.yml 2> /dev/null
+  fi
+}
+
+uninstall_zsh_config() {
+  if [ -L ~/.zshrc ]; then
+      echo -e "${BLUE}>>${NC} Zsh"
+      rm ~/.zshrc 2> /dev/null
+  fi
+}
+
+uninstall_tmux_config() {
+  if [ -L ~/.tmux.conf ]; then
+      echo -e "${BLUE}>>${NC} Tmux"
+      rm ~/.tmux.conf 2> /dev/null
+  fi
+}
+
+uninstall_git_config() {
+  if [ -L ~/.gitconfig ] && [ -L ~/.gitmessage ]; then
+      echo -e "${BLUE}>>${NC} Git"
+      rm ~/.gitconfig 2> /dev/null
+      rm ~/.gitmessage 2> /dev/null
+  fi
+}
+
+uninstall_helix_config() {
+  if [ -L ~/.config/helix ]; then
+      echo -e "${BLUE}>>${NC} Helix"
+      rm -rf ~/.config/helix 2> /dev/null
+  fi
+}
+
+if [ $uninstall_configurations -eq 1 ]; then
+  echo -e "${GREEN}>>${NC} Uninstalling configurations"
+  uninstall_alacritty_config
+  uninstall_zsh_config
+  uninstall_tmux_config
+  uninstall_git_config
+  uninstall_helix_config
 fi
 
-if [ -L ~/.config/helix ]; then
-    echo "helix"
-    rm -rf ~/.config/helix 2> /dev/null
+brew_uninstall() {
+  echo -e "${BLUE}>>${NC} Homebrew uninstall"
+  cat packages.txt | grep -v -e '^\s*$' | grep -v '#' | xargs brew uninstall
+}
+
+undo_tmux() {
+  if [ -f ~/.local/share/terminfo/74/tmux-256color ]; then
+    echo -e "${BLUE}>>${NC} Undo tmux setup"
+    rm ~/.local/share/terminfo/74/tmux-256color
+  fi
+}
+
+if [ $uninstall_packages -eq 1 ]; then
+  echo -e "${GREEN}>>${NC} Uninstalling packages"
+  brew_uninstall
+  undo_tmux
 fi
+
