aa323492ebae5813d08af3a3b27e1bb83502a4b3
David Bastien <velours.ingrate_00@icloud.com>
2023-12-12 09:02:37 -0500

    feat: highlight on yank

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,6 +1,19 @@
 require("set")
 require("remap")
 
+-- Highlight on yank
+local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
+})
+
 -- Setup lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
