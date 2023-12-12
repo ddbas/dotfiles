f110cda746cea77982e3161a2f110d61827a7c41
David Bastien <velours.ingrate_00@icloud.com>
2023-12-11 21:57:36 -0500

    improve the nvim configurations

--- home/.config/nvim/lua/ddbas/init.lua
+++
@@ -1,2 +0,0 @@
-require("ddbas.set")
-require("ddbas.remap")
---
+++ home/.config/nvim/lua/plugins/lsp-zero.lua
@@ -0,0 +1,73 @@
+return {
+  "VonHeikemen/lsp-zero.nvim",
+  branch = "v3.x",
+  dependencies = {
+    -- LSP support
+    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+
+    -- Autocompletion
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+  },
+  config = function()
+    -- lsp-zero.nvim
+    local lsp_zero = require("lsp-zero")
+    lsp_zero.on_attach(function(_, bufnr)
+      lsp_zero.default_keymaps({ buffer = bufnr })
+
+      local opts = { buffer = bufnr, remap = false }
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "<leader>k", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "gs", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>F", function() vim.lsp.buf.format() end, opts)
+    end)
+
+    -- mason.nvim
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        'astro',
+        'bashls',
+        'cssls',
+        'html',
+        'lua_ls',
+        'marksman',
+        'rust_analyzer',
+        'tailwindcss',
+        'tsserver'
+      },
+      handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+      }
+    })
+
+    -- nvim-cmp
+    local cmp = require('cmp')
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = {
+        { name = 'nvim_lsp' }
+      }
+    })
+  end
+}
