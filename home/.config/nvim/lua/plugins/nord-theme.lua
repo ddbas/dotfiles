f0c5e6c2a09c49bbaee856e2b2dda869536fcde1
David Bastien <velours.ingrate_00@icloud.com>
2023-12-17 17:57:24 -0500

    feat: fix LSP semantic tokens issue with nord theme

--- home/.config/nvim/lua/plugins/nord-theme.lua
+++ home/.config/nvim/lua/plugins/nord-theme.lua
@@ -3,6 +3,14 @@ return {
   name = "nord",
   priority = 1000,
   config = function()
+    -- https://github.com/shaunsingh/nord.nvim/issues/143
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+      end,
+    })
+
     vim.cmd.colorscheme "nord"
   end
 }
