d9a33a93621051254a895cfa28933ec57ac5afd4
David Bastien <velours.ingrate_00@icloud.com>
2024-01-01 10:50:37 -0500

    nord theme

--- home/.config/nvim/lua/plugins/catppuccin.lua
+++ home/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,24 +1,24 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000,
-  config = function()
-    local catppuccin = require("catppuccin")
-    catppuccin.setup({
-      flavour = "macchiato",
-      integrations = {
-        cmp = true,
-        mason = true,
-        native_lsp = {
-          enabled = true
-        },
-        telescope = {
-          enabled = true
-        },
-        treesitter = true
-      }
-    })
+  -- priority = 1000,
+  -- config = function()
+  --   local catppuccin = require("catppuccin")
+  --   catppuccin.setup({
+  --     flavour = "macchiato",
+  --     integrations = {
+  --       cmp = true,
+  --       mason = true,
+  --       native_lsp = {
+  --         enabled = true
+  --       },
+  --       telescope = {
+  --         enabled = true
+  --       },
+  --       treesitter = true
+  --     }
+  --   })
 
-    vim.cmd.colorscheme "catppuccin"
-  end
+  --   vim.cmd.colorscheme "catppuccin"
+  -- end
 }
--- home/.config/nvim/lua/plugins/nord-theme.lua
+++ home/.config/nvim/lua/plugins/nord-theme.lua
@@ -1,16 +1,16 @@
 return {
   "shaunsingh/nord.nvim",
   name = "nord",
-  -- priority = 1000,
+  priority = 1000,
   config = function()
     -- https://github.com/shaunsingh/nord.nvim/issues/143
-    -- vim.api.nvim_create_autocmd('LspAttach', {
-    --   callback = function(args)
-    --     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    --     client.server_capabilities.semanticTokensProvider = nil
-    --   end,
-    -- })
+    vim.api.nvim_create_autocmd('LspAttach', {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+      end,
+    })
 
-    -- vim.cmd.colorscheme "nord"
+    require('nord').set()
   end
 }
