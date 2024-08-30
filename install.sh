0eb87f849719c5e99c60829b2f33739ba087eed1
ddbas <velours.ingrate_00@icloud.com>
2024-08-29 20:15:55 -0400

    updated brew lockfile and install script

--- install.sh
+++ install.sh
@@ -4,14 +4,17 @@
 source ./env.sh
 
 # Stow the folders
-echo "--- Stowing folders ---"
+echo -e "\n--- Stowing folders ---\n"
 for folder in ${STOW_FOLDERS[@]}
 do
   echo "Stowing $folder"
   stow -R -t ~ $folder
 done
 
-# Install the packages
-echo "--- Installing packages ---"
+# Install brew packages
+echo -e "\n--- Brew install ---\n"
 brew bundle install
+
+# Install npm packages
+echo -e "\n--- Npm install ---\n"
 cd ~/.config/npm && npm ci
