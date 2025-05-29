ef16964ebda6d04e20c87aaa84f4cdf68bbfa2cf
David Bastien <david.bastien@autodesk.com>
2025-05-29 09:00:28 -0400

    refactor: nvim config updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,4 @@
-vim.g.mapleader = " "
-
--- Setup lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
-
-require("set")
-require("remap")
-require("autocmd")
+require("config.lazy")
+require("config.set")
+require("config.remap")
+require("config.autocmd")
