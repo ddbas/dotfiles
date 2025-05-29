cf280ff8ea6d0fc6b3bcd6439776f153b082ec85
ddbas <velours.ingrate_00@icloud.com>
2025-05-29 17:27:46 -0400

    refactor: cleanup telescope and treesitter configs

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,35 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	lazy = false,
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-        ensure_installed = {
-          'astro',
-          'css',
-          'dockerfile',
-          'html',
-          'javascript',
-          'json',
-          'lua',
-          'markdown',
-          'rust',
-          'terraform',
-          'tsx',
-          'typescript',
-          'vimdoc'
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false
-        }
-    })
+		configs.setup({
+			ensure_installed = {
+				"astro",
+				"css",
+				"dockerfile",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"rust",
+				"terraform",
+				"tsx",
+				"typescript",
+				"vimdoc",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		})
 
-    vim.treesitter.language.register('markdown', 'mdx')
-  end
+		vim.treesitter.language.register("markdown", "mdx")
+	end,
 }
