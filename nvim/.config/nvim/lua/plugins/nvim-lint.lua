e87f7ca2ba1ed265971d5aa7b99c3033678c4253
ddbas <velours.ingrate_00@icloud.com>
2025-05-29 17:17:20 -0400

    feat: improve nvim-lint config

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -6,11 +6,12 @@ return {
       javascript = {'eslint'},
       javascriptreact = {'eslint'},
       typescript = {'eslint'},
-      typescriptreact = {'eslint'}
+      typescriptreact = {'eslint'},
     }
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
       callback = function()
-        lint.try_lint(nil, { ignore_errors = true })
+        -- lint.try_lint(nil, { ignore_errors = true })
+        lint.try_lint()
       end,
     })
   end
