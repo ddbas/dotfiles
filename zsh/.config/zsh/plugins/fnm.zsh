5515dcf4a61efc43455767f6d2707003acc4395e
David Bastien <david.bastien@autodesk.com>
2025-05-29 10:04:27 -0400

    feat: have fnm only log errors

--- zsh/.config/zsh/plugins/fnm.zsh
+++ zsh/.config/zsh/plugins/fnm.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 if [ -x "$(command -v fnm)" ]; then
   # https://github.com/Schniz/fnm?tab=readme-ov-file#zsh
-  eval "$(fnm env --use-on-cd --shell zsh --version-file-strategy=recursive)"
+  eval "$(fnm env --use-on-cd --shell zsh --version-file-strategy=recursive --log-level=error)"
 fi
