bfdc08b839374ac859bb43eb46ad4eb7d52cc302
ddbas <velours.ingrate_00@icloud.com>
2025-08-04 21:33:58 -0400

    re-setup nvim-cmp

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,29 +1,29 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		vim.keymap.set("n", "<leader>k", function()
-			vim.lsp.buf.hover()
-		end, opts)
-		vim.keymap.set("n", "<leader>a", function()
-			vim.lsp.buf.code_action()
-		end, opts)
-		vim.keymap.set("n", "<leader>r", function()
-			vim.lsp.buf.rename()
-		end, opts)
-		vim.keymap.set("n", "<leader>d", function()
-			vim.diagnostic.open_float()
-		end, opts)
+  "neovim/nvim-lspconfig",
+  config = function()
+    vim.keymap.set("n", "<leader>k", function()
+      vim.lsp.buf.hover()
+    end, opts)
+    vim.keymap.set("n", "<leader>a", function()
+      vim.lsp.buf.code_action()
+    end, opts)
+    vim.keymap.set("n", "<leader>r", function()
+      vim.lsp.buf.rename()
+    end, opts)
+    vim.keymap.set("n", "<leader>d", function()
+      vim.diagnostic.open_float()
+    end, opts)
 
-		-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#pyright
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
     vim.lsp.enable('bashls')
-		vim.lsp.enable("biome")
-		vim.lsp.enable("cssls")
-		vim.lsp.enable("eslint")
-		vim.lsp.enable("html")
+    vim.lsp.enable("biome")
+    vim.lsp.enable("cssls")
+    vim.lsp.enable("eslint")
+    vim.lsp.enable("html")
     vim.lsp.enable("jsonls")
     vim.lsp.enable('lua_ls')
     vim.lsp.enable('marksman')
     vim.lsp.enable('rust_analyzer')
     vim.lsp.enable("ts_ls")
-	end,
+  end,
 }
