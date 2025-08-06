63a4b48ef03221952ad96a89dadcdcbb6f50f958
ddbas <velours.ingrate_00@icloud.com>
2025-08-05 22:37:16 -0400

    feat: update the theme

--- README.md
+++ README.md
@@ -18,4 +18,3 @@
 
 - catppuccin
 - everforest dark
-- deep space
--- nvim/.config/nvim/lua/plugins/nord.lua
+++ nvim/.config/nvim/lua/plugins/nord.lua
@@ -1,18 +1,6 @@
 return {
-  "shaunsingh/nord.nvim",
+  "arcticicestudio/nord-vim",
   name = "nord",
   enabled = false,
-  lazy = true,
-  config = function()
-    -- https://github.com/shaunsingh/nord.nvim/issues/143
-    vim.api.nvim_create_autocmd('LspAttach', {
-      callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        client.server_capabilities.semanticTokensProvider = nil
-      end,
-    })
-    vim.g.nord_disable_background = true
-
-    require('nord').set()
-  end
+  priority = 1000,
 }
