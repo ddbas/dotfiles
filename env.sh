00fe7f6ec0a7391074f94f7c41a0ebce731ebc84
ddbas <velours.ingrate_00@icloud.com>
2026-06-28 15:37:14 -0400

    chore: mise trust all repos

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-STOW_FOLDERS=(agents alacritty bat bin brewfile claude git moxide npm nvim opencode pi tmux zsh)
+STOW_FOLDERS=(agents alacritty bat bin brewfile claude git mise moxide npm nvim opencode pi tmux zsh)
